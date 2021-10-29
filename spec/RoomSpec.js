259f9b4421898faf92677eeb07788c03a992e5a9
Lewis Jones <lewis.jones1997@protonmail.com>
2021-10-29 09:30:05 +0100

    test(room): add test to ensure that room is available upon instantiation

--- spec/RoomSpec.js
+++ spec/RoomSpec.js
@@ -1,8 +1,15 @@
 describe('Room', () => {
   describe('Create a new room', () => {
-    it('Is created with a name', () => {
+    beforeEach(function() {
       room = new Room('Room One');
+    });
+
+    it('Is created with a name', () => {
       expect(room.name).toBe('Room One');
+    });
+
+    it('Is available by default', () => {
+      expect(room.available).toBe(true);
     });
   });
 });
