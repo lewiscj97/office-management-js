f1d3f9253dfdb3126e968e75ae17967a1a8d3d98
Lewis Jones <lewis.jones1997@protonmail.com>
2021-10-29 10:22:56 +0100

    test(office): add first test for empty rooms array

--- spec/OfficeSpec.js
+++ spec/OfficeSpec.js
@@ -5,11 +5,24 @@ describe('Room', () => {
     });
 
     it('Is created with a name', () => {
-      expect(room.name).toBe('Room One');
+      expect(room.name).toEqual('Room One');
     });
 
     it('Is available by default', () => {
       expect(room.available).toBe(true);
+    });
+  });
+});
+
+describe('Office', () => {
+  describe('Add meeting room', () => {
+    beforeEach(() => {
+      room = new Room('Room One');
+    });
+
+    it('Office has no meeting rooms initially', () => {
+      office = new Office();
+      expect(office.rooms).toEqual([]);
     });
   });
 });
