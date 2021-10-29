3b3c2d552796bc64a7d96a35a23a49b0dffbd6d5
Lewis Jones <lewis.jones1997@protonmail.com>
2021-10-29 12:14:35 +0100

    feat(*): webage shows all rooms and their details

--- src/interface.js
+++ src/interface.js
@@ -1,6 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const office = new Office();
-
   const hideNewRoomContainer = () => {
     const container = document.querySelector('#new-room-container')
     if (container.style.display === "none") {
@@ -8,7 +6,47 @@ document.addEventListener('DOMContentLoaded', () => {
     } else {
       container.style.display = "none";
     }
-  };
+  }
+
+  // Display rooms
+  const displayRooms = () => {
+    const container = document.querySelector('#rooms-container');
+    container.innerHTML = '';
+    const rooms = office.allMeetingRooms();
+    rooms.forEach((room, index) => {
+      // Create div element
+      const div = document.createElement('div');
+      div.setAttribute('id', index)
+      div.className = 'room';
+
+      // Room name
+      const roomName = document.createElement('p');
+      roomName.innerText += room.name;
+      roomName.className = 'room-name';
+      div.appendChild(roomName);
+
+      // availability
+      const availability = document.createElement('p');
+      availability.innerText += room.available ? 'Available' : 'Unavailable';
+      availability.className = 'room-availability';
+      div.appendChild(availability);
+
+      // booking
+      const booking = document.createElement('button');
+      booking.setAttribute('id', '#room-booking');
+      div.appendChild(booking);
+
+      // add div to container
+      container.appendChild(div);
+    });
+  }
+
+  // Populate office with some default rooms
+  const office = new Office();
+  office.addMeetingRoom('Room One');
+  office.addMeetingRoom('Room Two');
+  office.addMeetingRoom('Room Three');
+  displayRooms();
 
   // Show form for adding new room on click
   document.querySelector('#add-new-room').addEventListener('click', () => {
@@ -20,8 +58,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const roomName = document.querySelector('#room-name');
     
     office.addMeetingRoom(roomName.value);
-    console.log(`${roomName.value} added`)
+    console.log(`${roomName.value} added`);
     roomName.value = '';
     hideNewRoomContainer();
+    displayRooms();
   });
 });
